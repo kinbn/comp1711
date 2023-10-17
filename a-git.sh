e70a73632a216b49606c8378965054bd4298b932
Inban Kanmani <146678142+kinbn@users.noreply.github.com>
2023-10-17 22:10:46 +0000

    Git automation

---
+++ a-git.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+ echo "                                                                           # # # # # #  # #### #  # #### #  "
+ echo "                                                                          #          #     ##        ##     "
+ echo "                                                                          #                ##        ##     "
+ echo "                                                                          #                ##        ##     "
+ echo "                                                                          #      # # #     ##        ##     "
+ echo "                                                                          #          #     ##        ##     "
+ echo "                                                                          #          #     ##        ##     "
+ echo "                                                                          # # # # # #   # #### #     ##     "
+
+
+
+
+git status
+
+echo ""
+echo ""
+echo ""
+
+ echo " Name of the file(s): "
+ read  gitadd
+
+git add $gitadd
+
+
+echo ""
+echo ""
+echo ""
+
+echo "Enter a message: "
+
+read gitcommit
+
+git commit -m "$gitcommit" 
+
+echo ""
+echo ""
+echo ""
+
+echo " Shall I push? [y/n] "
+
+read pushquery
+
+
+if [[ "$pushquery" = "y" ]]; 
+then
+
+    git push
+
+else  
+    echo " bruh "
+
+fi
